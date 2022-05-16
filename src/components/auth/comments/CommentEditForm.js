0e361c82167717d528692f55302e8d186cf68878
Vishal Trivedi <vishaltrivedi92@gmail.com>
2022-05-16 14:37:50 -0500

    ReadMe

--- src/components/auth/comments/CommentEditForm.js
+++ src/components/auth/comments/CommentEditForm.js
@@ -9,21 +9,26 @@ export const EditCommentToPageCard = ({ getLoggedInUser }) => {
     const navigate = useNavigate();
     //to navigate to specific page
 
+
+    //copy of useState. updating that copy with new information from the user. setting that useState to that copy. 
     const controlInput = (event) => {
         const newComics = { ...comics }
+        //3. copy of comics
+
         newComics.userId = getLoggedInUser()
         //3. setting the id in newComics to the logged in user
 
         let selectedTarget = event.target.value
-        // storing event.target.value to selectedTarget
+        // storing the input field 
+
 
         if (event.target.id.includes('Id')) {
             selectedTarget = parseInt(selectedTarget)
         }
-        //getting slected comic by id and parsing storing in selectedTarget
+       
 
         newComics[event.target.id] = selectedTarget
-        // storing selectedTarget to newComics square bracket object notation
+         // storing whats typed in the input field 
 
         setComics(newComics)
         //setting the usedState equal to the updated object of comics
@@ -36,7 +41,7 @@ export const EditCommentToPageCard = ({ getLoggedInUser }) => {
     }, [])
 
 
-    const editComics = (event) => {
+    const saveComics = (event) => {
         event.preventDefault()
         updateFavoriteComic(comics)
             .then(() => navigate('/Favorites'))
@@ -53,7 +58,7 @@ export const EditCommentToPageCard = ({ getLoggedInUser }) => {
                     <label htmlFor="name">Comment</label>
                     <input type="text" id="comments" onChange={controlInput} required autoFocus className="controlled_form comment_controlled_form" value={comics.comments} />
                 </fieldset>
-                <button type="button" id="comment_submit_btn" className="submit_btn" onClick={editComics}>Submit</button>
+                <button type="button" id="comment_submit_btn" className="submit_btn" onClick={saveComics}>Submit</button>
             </form>
         </div>
     )// 2. on change once something in the inputform is typed controlInput is activated
